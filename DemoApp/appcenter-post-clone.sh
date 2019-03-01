e217a99caa32fd6b69c659e6ff10a55de7e257d5
shawndyas <47836561+shawndyas@users.noreply.github.com>
2019-02-28 16:19:33 -0800

    Update appcenter-post-clone.sh

--- DemoApp/appcenter-post-clone.sh
+++ DemoApp/appcenter-post-clone.sh
@@ -3,3 +3,10 @@ echo "Executing post clone script in `pwd`"
 echo $NPM_RC | base64 --decode > $APPCENTER_SOURCE_DIRECTORY/DemoApp/.npmrc
 # Delete everything except DemoApp folder
 rm -rf ../appcenter* ../AppCenterReactNativeShared ../TestApp34 ../BrownfieldTestApp ../TestApp
+
+NODE_VERSION="10.15.1"
+npm config delete prefix
+. ~/.bashrc
+nvm install "$NODE_VERSION"
+nvm unalias default
+nvm alias node8 "$NODE_VERSION"
